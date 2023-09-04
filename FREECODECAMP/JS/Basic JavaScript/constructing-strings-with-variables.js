b77fe762faebe9aa9ab28f1025d92db0808e315e
Dmitry Shamko <proffery@gmail.com>
2023-09-04 16:21:25 +0300

    50%

---
+++ FREECODECAMP/JS/Basic JavaScript/constructing-strings-with-variables.js
@@ -0,0 +1,3 @@
+// Only change code below this line
+const myName = "Dmitry";
+const myStr = "My name is " + myName + " and I am well!";
---
+++ FREECODECAMP/JS/Basic JavaScript/shopping-list.js
@@ -0,0 +1,6 @@
+const myList = [];
+myList.push(["Chocolate Bar", 15]);
+myList.push(["Choco Bar", 14]);
+myList.push(["Chocol Bar", 13]);
+myList.push(["Chocola Bar", 12]);
+myList.push(["Chocolat Bar", 11]);
