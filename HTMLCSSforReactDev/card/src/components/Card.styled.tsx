762aa656dbe7f58f39ed289b84bd198536cbe534
Dmitry Shamko <proffery@gmail.com>
2023-09-13 12:16:14 +0300

    card

---
+++ HTMLCSSforReactDev/card/src/components/Card.styled.tsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export const Card = styled.div`
+  min-width: 300px;
+  min-height: 350px;
+  border-radius: .5rem;
+  box-shadow: 0px 4px 20px 5px  rgba(0, 0, 0, 0.1);
+  padding: .5rem;
+  background-color: #fff;
+`
